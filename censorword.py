55ae9e6dde28db8f28cd1f7d081494724f5cf728
sugafree8 <ashmaghimire8@gmail.com>
2022-04-19 18:49:58 +0545

    homework uploaded

---
+++ censorword.py
@@ -0,0 +1,18 @@
+#censoring a word
+ 
+text = input("Enter your text: ").lower()
+word = input("Choose a word from the previously entered text to be censored: ").lower()
+
+def censor(text,word):
+    t=text.split()
+    n=[]
+    for i in t:
+        if(i==word):
+            n.append("*"*len(word))
+        else:
+            n.append(i)
+    return " ".join(n)
+   
+file = open("censored.txt","w")
+file.write(censor(text,word))
+
